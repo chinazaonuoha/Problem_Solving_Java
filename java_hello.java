5c6301d959fd80e1be962594b6a901310dfb9d69
chinazaonuoha <developerworkpace@gmail.com>
2023-09-28 09:22:12 +0100

    Setting up the working evironment

---
+++ java_hello.java
@@ -0,0 +1,10 @@
+/*
+ * My first Hello World Progarm in Linux with Java
+ */
+class Hello{
+   // Java is crazy
+   public static void main(String args[]){
+       System.out.println("Hello Java in Linux");
+   }
+}
+
